4385cfeb87a55d9dcca799ada25f07bd8a446c9d
Hillary <casscainbatgirl@gmail.com>
2017-04-26 21:17:18 -0400

    following a TS canvas tutorial

--- canvasgame.js
+++ canvasgame.js
@@ -1,10 +1,43 @@
 var canvas;
 var ctx;
+var cCircle = (function () {
+    function cCircle(x, y, radius, color, line_width) {
+        if (color === void 0) { color = "red"; }
+        if (line_width === void 0) { line_width = 2; }
+        var _this = this;
+        this.x = 0;
+        this.y = 0;
+        this.radius = 10;
+        this.lineWidth = 2;
+        this.color = "red";
+        this.draw = function () {
+            ctx.save();
+            ctx.beginPath();
+            ctx.strokeStyle = _this.color;
+            ctx.lineWidth = _this.lineWidth;
+            ctx.arc(_this.x, _this.y, _this.radius, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.restore();
+        };
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.lineWidth = line_width;
+    }
+    return cCircle;
+}());
+var circle1 = new cCircle(200, 300, 50);
+var circle2 = new cCircle(400, 550, 150, "blue", 5);
 function gameLoop() {
     requestAnimationFrame(gameLoop);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 1280, 720);
+    circle1.draw();
+    circle2.draw();
 }
 window.onload = function () {
-    canvas = document.getElementById('cnvs');
+    canvas = document.getElementById('screen');
     ctx = canvas.getContext("2d");
     gameLoop();
 };
--- canvasgame.ts
+++ canvasgame.ts
@@ -1,12 +1,52 @@
 var canvas: HTMLCanvasElement;
 var ctx: CanvasRenderingContext2D;
 
+interface iShape {
+	  draw(): void;
+	  x: number;
+	  y: number;
+	  color: string;
+	  lineWidth: number;
+}
+
+class cCircle implements iShape {
+      public x: number = 0;
+      public y: number = 0;
+      public radius: number = 10;
+      public lineWidth: number = 2;
+      public color: string = "red";
+      constructor(x: number, y: number, radius: number, color: string = "red", line_width: number =2)
+      {
+	this.x = x;
+	this.y = y;
+	this.radius = radius;
+	this.color = color;
+	this.lineWidth = line_width;
+	}
+	public draw = (): void => {
+	       ctx.save();
+	       ctx.beginPath();
+	       ctx.strokeStyle = this.color;
+	       ctx.lineWidth = this.lineWidth;
+	       ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+	       ctx.stroke();
+	       ctx.restore();
+	      }
+	     }
+
+var circle1: cCircle = new cCircle(200, 300, 50);
+var circle2: cCircle = new cCircle(400, 550, 150, "blue", 5);
+
 function gameLoop(){
 	 requestAnimationFrame(gameLoop);
+	 ctx.fillStyle = "black";
+	 ctx.fillRect(0,0,1280,720);
+	 circle1.draw();
+	 circle2.draw();
 	 }
 
 window.onload = () => {
-	      canvas = <HTMLCanvasElement>document.getElementById('cnvs');
+	      canvas = <HTMLCanvasElement>document.getElementById('screen');
 	      ctx = canvas.getContext("2d");
 	      gameLoop();
 	      }
